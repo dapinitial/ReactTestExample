1b804a7e689069df6cbf46b27cc82c9a66251796
David Puerto <me@davidpuerto.com>
2017-06-04 21:15:35 -0700

    continuing to test commentBox pulling into App.js

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,12 +3,13 @@ import App from '../../src/components/app';
 
 // Use 'describe' to group together similar tests
 describe('App', () => {
+  let component;
+  beforeEach(() => {
+    component = renderComponent(App);
+  })
   // Use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-    const component = renderComponent(App);
+  it('shows a comment box', () => {
     // Use 'expect' to make an assertion about a target
-    expect(component).to.contain('React simple starter');
-
+    expect(component.find('.comment-box')).to.exist;
   });
-
 });
